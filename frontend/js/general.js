d41f4d65a51c46ccf4544825440b96583ccd0c0a
Zarina-2022 <sekerdagzarina@gmail.com>
2023-01-07 22:44:24 +0100

    close response model

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -19,6 +19,8 @@ const responseContainer = document.getElementById('responseContainer')
 const submitBtn = document.getElementById('submitBtn')
 const responseText = document.getElementById('responseText')
 
+const closeResponse = document.getElementById('closeResponse')
+
 // INPUTS
 // nameInput = focusin/focusout
 nameInput.addEventListener('focusin', () => {
@@ -98,6 +100,9 @@ contactForm.addEventListener('submit', (event) => {
                 responseContainer.style.display = 'block'
                 responseContainer.classList.add('responseSuccess')
                 responseText.innerText = 'Your form has been successfully submitted.'
+
+                /*
+                // setTimeout ile ekrandaki tum bilgileri temizlemek:
                 setTimeout(() => {
                     responseContainer.style.display = 'none'
                     responseContainer.classList.remove('responseSuccess')
@@ -110,6 +115,7 @@ contactForm.addEventListener('submit', (event) => {
                     emailInput.value = ''
                     messageArea.value = ''
                 }, 3000);
+                */
             }
         })
         .catch(err => {
@@ -117,6 +123,8 @@ contactForm.addEventListener('submit', (event) => {
             responseContainer.style.display = 'block'
             responseContainer.classList.add('responseFail')
             responseText.innerText = 'An error occurred while submitting your form.'
+            /*
+            // setTimeout ile ekrandaki tum bilgileri temizlemek:
             setTimeout(() => {
                 responseContainer.style.display = 'none'
                 responseContainer.classList.remove('responseSuccess')
@@ -125,7 +133,26 @@ contactForm.addEventListener('submit', (event) => {
                 submitBtn.classList.replace('submitBtnDisabled', 'submitBtnActive')
                 submitBtn.innerHTML = 'Send'
             }, 3000);
+            */
         })
 })
+    // Carpi iconuna tiklayarak ekrandaki bilgileri temizleme:
+ closeResponse.addEventListener("click", () => {
+    responseContainer.style.display = 'none'
+    responseContainer.classList.remove('responseSuccess')
+    responseText.innerText = ''
+    submitBtn.disabled = false
+    submitBtn.classList.replace('submitBtnDisabled', 'submitBtnActive')
+    submitBtn.innerHTML = 'Send'
+    nameInput.value = ''
+    surnameInput.value = ''
+    emailInput.value = ''
+    messageArea.value = ''
+ });
+
 
 // google = fetch post example yaz ornekler var orda 
+
+
+
+
